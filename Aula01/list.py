275901b2e613060e9b1a7d68b2da1eaf30ed267a
italorafaeltavares <italorafaeltavares@gmail.coom>
2022-01-12 08:58:49 -0300

    Add list.py

---
+++ Aula01/list.py
@@ -0,0 +1,28 @@
+# Dados sequenciais
+# List
+games = ["God of War", "Mario Bros", "Donkey Kong", "Mortal kombat"]
+gameSnes = games[1]
+
+print(gameSnes)
+print("Melhor jogo da Playstation é ", games[0])
+
+# Sublist
+categoryGames = [["God of War", "Mortal kombat"], ["Mario Bros", "Donkey Kong"]]
+
+gameSony = categoryGames[0]
+gameSonyFigth = gameSony[1]
+
+print(gameSony)
+print(gameSonyFigth)
+
+
+# Dados diferentes: tudoJuntoMisturado = ['Test', 3, 10.25, True]
+
+name = 'Italo'
+age = 33
+height = 1.75
+hobbyGames = True
+
+myList = [name, age, height, hobbyGames]
+print(f"Eu me chamo {myList[0]} e tenho {myList[1]} anos de idade")
+
